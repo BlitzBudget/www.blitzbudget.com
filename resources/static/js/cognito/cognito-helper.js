ba97e50598bc01811c3d52e6abad7eb7d34cb14e
Nagarjun <nagarjun_nagesh@outlook.com>
2019-10-23 15:46:17 +0200

    Configure cognito register scenario

--- resources/static/js/cognito/cognito-helper.js
+++ resources/static/js/cognito/cognito-helper.js
@@ -1,131 +1,210 @@
 /*global AWSCogUser _config*/
 
-var AWSCogUser = window.AWSCogUser || {};
-AWSCogUser.map = AWSCogUser.map || {};
+uh = {
+		// Fetch User From Storage
+		fetchUserFromLocalStorage() {
+			
+			// Configure the pool data from the config.js
+			let poolData = {
+		        UserPoolId: _config.cognito.userPoolId,
+		        ClientId: _config.cognito.userPoolClientId
+		    };
 
-(function rideScopeWrapper($) {
-    var authToken;
-    AWSCogUser.authToken.then(function setAuthToken(token) {
-        if (token) {
-            authToken = token;
-        } else {
-            window.location.href = '/login';
-        }
-    }).catch(function handleTokenError(error) {
-        alert(error);
-        window.location.href = '/login';
-    });
-    
-}(jQuery));
-//    function requestUnicorn(pickupLocation) {
-//        $.ajax({
-//            method: 'POST',
-//            url: _config.api.invokeUrl + '/ride',
-//            headers: {
-//                Authorization: authToken
-//            },
-//            data: JSON.stringify({
-//                PickupLocation: {
-//                    Latitude: pickupLocation.latitude,
-//                    Longitude: pickupLocation.longitude
-//                }
-//            }),
-//            contentType: 'application/json',
-//            success: completeRequest,
-//            error: function ajaxError(jqXHR, textStatus, errorThrown) {
-//                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
-//                console.error('Response: ', jqXHR.responseText);
-//                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
-//            }
-//        });
-//    }
-//
-//    function completeRequest(result) {
-//        var unicorn;
-//        var pronoun;
-//        console.log('Response received from API: ', result);
-//        unicorn = result.Unicorn;
-//        pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
-//        displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.');
-//        animateArrival(function animateCallback() {
-//            displayUpdate(unicorn.Name + ' has arrived. Giddy up!');
-//            AWSCogUser.map.unsetLocation();
-//            $('#request').prop('disabled', 'disabled');
-//            $('#request').text('Set Pickup');
-//        });
-//    }
-//
-//    // Register click handler for #request button
-//    $(function onDocReady() {
-//        $('#request').click(handleRequestClick);
-//        $('#signOut').click(function() {
-//            AWSCogUser.signOut();
-//            alert("You have been signed out.");
-//            window.location = "signin.html";
-//        });
-//        $(AWSCogUser.map).on('pickupChange', handlePickupChanged);
-//
-//        AWSCogUser.authToken.then(function updateAuthMessage(token) {
-//            if (token) {
-//                displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
-//                $('.authToken').text(token);
-//            }
-//        });
-//
-//        if (!_config.api.invokeUrl) {
-//            $('#noApiMessage').show();
-//        }
-//    });
-//
-//    function handlePickupChanged() {
-//        var requestButton = $('#request');
-//        requestButton.text('Request Unicorn');
-//        requestButton.prop('disabled', false);
-//    }
-//
-//    function handleRequestClick(event) {
-//        var pickupLocation = AWSCogUser.map.selectedPoint;
-//        event.preventDefault();
-//        requestUnicorn(pickupLocation);
-//    }
-//
-//    function animateArrival(callback) {
-//        var dest = AWSCogUser.map.selectedPoint;
-//        var origin = {};
-//
-//        if (dest.latitude > AWSCogUser.map.center.latitude) {
-//            origin.latitude = AWSCogUser.map.extent.minLat;
-//        } else {
-//            origin.latitude = AWSCogUser.map.extent.maxLat;
-//        }
-//
-//        if (dest.longitude > AWSCogUser.map.center.longitude) {
-//            origin.longitude = AWSCogUser.map.extent.minLng;
-//        } else {
-//            origin.longitude = AWSCogUser.map.extent.maxLng;
-//        }
-//
-//        AWSCogUser.map.animate(origin, dest, callback);
-//    }
-//
-//    function displayUpdate(text) {
-//        $('#updates').append($('<li>' + text + '</li>'));
-//    }
-//}(jQuery));
+		    let userPool;
 
-/**
- * Check if user is authenticated in Amazon Cognito
- * 
- * @returns
- */
-function isUserAuthenticated() {
-	window.AWSCogUser.authToken.then(function setAuthToken(token) {
-        if (token) {
-            return true;
-        } else {
-        	return false;
-        }
-    }).catch(function handleTokenError(error) {
-        return false;
-    });
+		    // If the config for the cognito is missing
+		    if (!(_config.cognito.userPoolId &&
+		          _config.cognito.userPoolClientId &&
+		          _config.cognito.region)) {
+		    	showNotification('There is an error configuring the user access. Please contact support!','top','center','danger');
+		        return;
+		    }
+		    
+			// Get the user pool from Cognito
+		    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+		    if (typeof AWSCognito !== 'undefined') {
+		        AWSCognito.config.region = _config.cognito.region;
+		    }
+		    
+		    return userPool;
+		},
+
+		// Show a login popup if not logged in
+		checkIfUserLoggedIn() {
+			
+			// Fetch user from local storage
+			let userPool = uh.fetchUserFromLocalStorage();
+		    
+		    let cognitoUser = userPool.getCurrentUser();
+		    
+		    if (cognitoUser != null) {
+		        cognitoUser.getSession(function(err, session) {
+		            if (err) {
+		            	er.sessionExpiredSwal(true);
+		            }
+		            // Session is valid
+		            return true;
+		        });
+		    } else {
+		    	er.sessionExpiredSwal(true);
+		    }
+		    
+		    return false;
+		},
+
+		// Signout the user and redirect to home page
+		signoutUser(globally) {
+			
+			// Fetch user from local storage
+			let userPool = uh.fetchUserFromLocalStorage();
+			let cognitoUser = userPool.getCurrentUser();
+			let homepageUrl = 'https://www.blitzbudget.com';
+			
+			if(cognitoUser != null) {
+				// Signout user from cognito
+				if(globally) {
+					cognitoUser.globalSignout();
+				} else {
+					cognitoUser.signOut();
+				}
+			}
+			
+			// redirect user to home page
+			window.location.href = homepageUrl;
+		},
+		
+		// Signout the user and redirect to home page
+		signoutGlobally() {
+			uh.signoutUser(true);
+		},
+
+		// Delete a User
+		deleteUser() {
+			
+			// Fetch user from local storage
+			let userPool = uh.fetchUserFromLocalStorage();
+			let cognitoUser = userPool.getCurrentUser();
+			
+			cognitoUser.deleteUser(function(err, result) {
+		        if (err) {
+		            alert(err);
+		            return;
+		        }
+		        console.log('call result: ' + result);
+		    });
+		},
+
+		// Forgot Password Flow
+		forgotPassword() {
+			
+			// Fetch user from local storage
+			let userPool = uh.fetchUserFromLocalStorage();
+			let cognitoUser = userPool.getCurrentUser();
+			
+			// TODO Adopt Code
+			cognitoUser.forgotPassword({
+		        onSuccess: function (result) {
+		            console.log('call result: ' + result);
+		        },
+		        onFailure: function(err) {
+		            alert(err);
+		        },
+		        inputVerificationCode() {
+		            var verificationCode = prompt('Please input verification code ' ,'');
+		            var newPassword = prompt('Enter new password ' ,'');
+		            cognitoUser.confirmPassword(verificationCode, newPassword, this);
+		        }
+			 });
+		},
+
+		// Change Password Flow
+		changePassword(oldPassword, newPassword) {
+			// TODO Adopt Code
+			cognitoUser.changePassword(oldPassword, newPassword, function(err, result) {
+		        if (err) {
+		            alert(err);
+		            return;
+		        }
+		        console.log('call result: ' + result);
+		    });
+		},
+
+		// Resend Confirmation Code
+		resendConfirmationCode() {
+			// TODO Adopt Code
+			cognitoUser.resendConfirmationCode(function(err, result) {
+		        if (err) {
+		            alert(err);
+		            return;
+		           }
+		           alert(result);
+		    });
+		},
+
+		// Update User Attributes
+		updateUserAttributes() {
+			// TODO Adopt Code
+			let attributeList = [];
+			let attribute = {
+		        Name : 'nickname',
+		        Value : 'joe'
+		    };
+			attribute = new AmazonCognitoIdentity.CognitoUserAttribute(attribute);
+		    attributeList.push(attribute);
+
+		    cognitoUser.updateAttributes(attributeList, function(err, result) {
+		        if (err) {
+		            alert(err);
+		            return;
+		        }
+		        console.log('call result: ' + result);
+		    });
+		},
+
+		// Verify an Attribute
+		verifyAnAttirbute() {
+			// TODO Adopt Code
+			cognitoUser.getAttributeVerificationCode('email', {
+		        onSuccess: function (result) {
+		            console.log('call result: ' + result);
+		        },
+		        onFailure: function(err) {
+		            alert(err);
+		        },
+		        inputVerificationCode: function() {
+		        	let verificationCode = prompt('Please input verification code: ' ,'');
+		            cognitoUser.verifyAttribute('email', verificationCode, this);
+		        }
+			 });
+		},
+
+		// Retrieve Attributes
+		retrieveAttributes() {
+			// Fetch user from local storage
+			let userPool = uh.fetchUserFromLocalStorage();
+			let cognitoUser = userPool.getCurrentUser();
+			
+			cognitoUser.getSession(function (err, session) {
+				cognitoUser.getUserAttributes(function(err, result) {
+			        if (err) {
+			        	showNotification('The Following Error has been encournter: ' + err);
+			            return;
+			        }
+			        for (i = 0; i < result.length; i++) {
+			        	let name = result[i].getName();
+			        	if(name.includes('custom:')) {
+			        		// if custom values then remove custom: 
+			        		let elemName = lastElement(splitElement(name,':'));
+			        		currentUser[elemName] = result[i].getValue();
+			        	} else {
+			        		currentUser[name] = result[i].getValue();
+			        	}
+			        }
+			    });
+			});
+		}
 }
+
+//Loads the current Logged in User
+uh.retrieveAttributes();
