c48d9375019de24931529e9a5b2835ef07d38b49
Arjun <nagarjun_nagesh@outlook.com>
2020-02-28 23:45:49 +0100

    mobile responsiveness

--- resources/static/js/plugins/libraries.js
+++ resources/static/js/plugins/libraries.js
@@ -119,23 +119,19 @@ function buildPieChart(dataPreferences, id, absoluteTotal) {
 	  donut: true,
 	  donutWidth: 25,
 	  startAngle: 270,
-	  showLabel: false,
-	  height: '12.75rem'
+	  showLabel: false
     };
     
     let responsiveOptions = [
-	  ['screen and (min-width: 640px)', {
-	    chartPadding: 80
+	  ['screen and (max-width: 640px)', {
+	    chartPadding: 60
 	  }],
-	  ['screen and (min-width: 1301px)', {
-	    labelOffset: 30,
-	    chartPadding: 10
-	  }],
-	  ['screen and (min-width: 992px)', {
-  	    labelOffset: 45,
-  	    chartPadding: 40,
+	  ['screen and (max-width: 992px)', {
+  	    chartPadding: 40
   	  }],
-	  
+  	  ['screen and (max-width: 1301px)', {
+	    chartPadding: 10
+	  }] 
 	];
     
     // Reset the chart
